563cdeaafb18babeda08636547f1d1d501f70cec
ser-cal <marcello.calisto@tbz.ch>
2021-01-23 11:30:01 +0100

    Update install.sh - node.js, git und postman-repo ergänzt
    Ergänzend noch node.js, git und ein Repo für Postman von taylonr ergänzt

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,11 @@
 #   
 #  Leere VM mit einer Intro Seite
 #
-
+# Install apache, php, ftp, powershell, markdown to HTML
+sudo apt update
+sudo apt install -y php libapache2-mod-php vsftpd markdown
+sudo apt install -y nodejs git
+sudo snap install powershell --classic
+git clone https://github.com/taylonr/postman.git
 # Introseite
 bash -x /opt/lernmaas/helper/intro
